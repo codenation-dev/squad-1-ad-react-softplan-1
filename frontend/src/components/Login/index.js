5f7341d0387628e6c1b10f5b21460004f56f71fe
antoniojunioalves <antoniojunioalves@gmail.com>
2019-11-18 20:03:27 -0300

    Iniciando a aplicação de front-end

---
+++ frontend/src/components/Login/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonToolbar from "react-bootstrap/ButtonToolbar";
+
+const Login = () => {
+  return (
+    <div>
+      <ButtonToolbar>
+        <Button variant="primary">Primary</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="success">Success</Button>
+        <Button variant="warning">Warning</Button>
+        <Button variant="danger">Danger</Button>
+        <Button variant="info">Info</Button>
+        <Button variant="light">Light</Button>
+        <Button variant="dark">Dark</Button>
+        <Button variant="link">Link</Button>
+      </ButtonToolbar>
+    </div>
+  );
+};
+
+export default Login;
