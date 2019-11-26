121ad9e5429c834396ed18015fc1615446871099
antoniojunioalves <antoniojunioalves@gmail.com>
2019-11-26 14:04:31 -0300

    Criação da tela de login.

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -1,21 +1,16 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import ButtonToolbar from "react-bootstrap/ButtonToolbar";
+import "./Login.css";
 
 const Login = () => {
   return (
-    <div>
-      <ButtonToolbar>
-        <Button variant="primary">Primary</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="success">Success</Button>
-        <Button variant="warning">Warning</Button>
-        <Button variant="danger">Danger</Button>
-        <Button variant="info">Info</Button>
-        <Button variant="light">Light</Button>
-        <Button variant="dark">Dark</Button>
-        <Button variant="link">Link</Button>
-      </ButtonToolbar>
+    <div className="login-centralizar">
+      <div className="login-main">
+        <h1>Login</h1>
+        <input type="text" placeholder="e-mail"></input>
+        <input type="password" placeholder="password"></input>
+        <button className="submit-btn">Login</button>
+        <a href="/">Esqueci a senha</a>
+      </div>
     </div>
   );
 };
