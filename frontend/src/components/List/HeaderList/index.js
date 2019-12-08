41fd761a63d89152a0a2bdde1d6374c23101954a
Daniel de Souza Martins <xitaocrazy@gmail.com>
2019-12-08 20:44:58 -0300

    Update index.js

--- frontend/src/components/List/HeaderList/index.js
+++ frontend/src/components/List/HeaderList/index.js
@@ -18,38 +18,38 @@ const HeaderList = (props) => {
 
   return (
     <>
-    <InputGroup className="mb-3">    
-      <ButtonToolbar >
-        <SelectedList
-          title="Origem"
-          options={["Todas", "Produção", "Homologação", "Dev"]}
-          handleChange={props.changeAmbiente}
-        />
-        <SelectedList
-          title="Ordenar por"
-          options={["Nenhum", "Level", "Frequência"]}
-          handleChange={props.changeOrderBy}
-        />
-        <SelectedList
-          title="Buscar por"
-          options={["Todos", "Level", "Descrição", "Origem"]}
-          handleChange={props.changeSearchBy}
-        />
-        <InputGroup>
-          <Form noValidate onSubmit={handleSubmit}>
-            <FormControl
-              placeholder="Filtrar por..."
-              value={filtro}
-              onChange={handleChange}
-            />
-          </Form>        
-          <InputGroup.Append>
-            <InputGroup.Text>Pesquisar</InputGroup.Text>
-          </InputGroup.Append>
-        </InputGroup>
-      </ButtonToolbar>
-    </InputGroup>
-    <ButtonToolbar className="mb-3">
+      <InputGroup className="mb-3">    
+        <ButtonToolbar >
+          <SelectedList
+            title="Origem"
+            options={["Todas", "Produção", "Homologação", "Dev"]}
+            handleChange={props.changeAmbiente}
+          />
+          <SelectedList
+            title="Ordenar por"
+            options={["Nenhum", "Level", "Frequência"]}
+            handleChange={props.changeOrderBy}
+          />
+          <SelectedList
+            title="Buscar por"
+            options={["Todos", "Level", "Descrição", "Origem"]}
+            handleChange={props.changeSearchBy}
+          />
+          <InputGroup>
+            <Form noValidate onSubmit={handleSubmit}>
+              <FormControl
+                placeholder="Filtrar por..."
+                value={filtro}
+                onChange={handleChange}
+              />
+            </Form>        
+            <InputGroup.Append>
+              <InputGroup.Text>Pesquisar</InputGroup.Text>
+            </InputGroup.Append>
+          </InputGroup>
+        </ButtonToolbar>
+      </InputGroup>
+      <ButtonToolbar className="mb-3">
         <Button
           variant="light"
           onClick={props.archivedSelected}
