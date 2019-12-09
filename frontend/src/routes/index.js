6d8d88f765b3bb9e29cd93209526922217d14d31
antoniojunioalves <antoniojunioalves@gmail.com>
2019-12-08 22:54:51 -0300

    Criando componente de details

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,12 +3,14 @@ import { Switch, Route, Redirect } from "react-router-dom";
 
 import { Home } from "../pages/Home";
 import { Error } from "../pages/Error";
+// import { ErrorDetails } from "../components/ErrorDetails";
 import { NotFound } from "../components/NotFound";
+import List from "../components/List";
 
 const Routes = () => (
   <>
     <Switch>
-      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/" component={List}></Route>
       <Route exact path="/error/:id" component={Error}></Route>
       <Route exact path="/404" component={NotFound}></Route>
       <Route path="*" render={() => <Redirect to="/404" />}></Route>
