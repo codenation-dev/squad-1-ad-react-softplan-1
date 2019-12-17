7d5e85ff4eb6437782927147eab462f9a727cd3b
Daniel de Souza Martins <xitaocrazy@gmail.com>
2019-12-16 21:28:54 -0300

    Update Header.js

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -3,6 +3,7 @@ import { Navbar } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faReact } from "@fortawesome/free-brands-svg-icons";
 import UserMenu from "./UserMenu.js";
+import { Link } from "react-router-dom";
 
 const Header = () => (
   <Navbar
@@ -12,13 +13,15 @@ const Header = () => (
     expand="lg"
     className="d-flex align-items-center justify-content-between"
   >
-    <Navbar.Brand href="/">
-      <FontAwesomeIcon
-        icon={faReact}
-        size="2x"
-        className="d-inline-block align-middle"
-      />{" "}
+    <Navbar.Brand>
+      <Link to="/">
+        <FontAwesomeIcon
+          icon={faReact}
+          size="2x"
+          className="d-inline-block align-middle"
+        />{" "}
         Central de Erros
+      </Link>
     </Navbar.Brand>
     <UserMenu />
   </Navbar>
