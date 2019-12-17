7cebfe57851b38d7fd3309db3b553d7acd10ff21
antoniojunioalves <antoniojunioalves@gmail.com>
2019-12-17 11:13:26 -0300

    Corrigindo backend para publicar

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/app', {
+mongoose.connect(`mongodb+srv://${process.env.USER_MONGO}:${process.env.PASSWORD_MONGO}@cluster0-jwbee.mongodb.net/${process.env.NAME_DB}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
